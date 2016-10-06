ae4bfa04a0550b9c32886a6dd1267231928f5463
rBBr <reubbers@gmail.com>
2016-10-06 16:16:04 -0300

    Update FilaEncadeada.java

--- Trabalhos_ED/src/Filas_autoreferenciadas/FilaEncadeada.java
+++ Trabalhos_ED/src/Filas_autoreferenciadas/FilaEncadeada.java
@@ -3,78 +3,76 @@ package cap3.rbbr.autoreferencia;
 
 public class FilaEncadeada {
 	
-	private Celula frente;
-	private Celula tras;
-
-	public FilaEncadeada () {
-		this.frente = new Celula();
-		this.tras = this.frente;
-		this.frente.prox= null;
 		
-	
-	}	
-		
-	
-	public void enfilerar(Object elemento) {
-			tras.setProx(new Celula());
-			tras = tras.getProx();
-			tras.setValor(elemento);
-			
-			
-	   }
+		private Celula frente;
+		private Celula tras;
 
-	
-	public Object desenfilerar() throws Exception {
-			Object valor = null;
-			if(this.vazia()){
-				throw new Exception ("error: fila vazia!!");
+		public FilaEncadeada () {
+			this.frente = new Celula();
+			this.tras = this.frente;
+			this.frente.prox= null;
+			
+		
+		}	
+			
+		
+		public void enfilerar(Object elemento) {
+				tras.prox=new Celula();
+				tras = tras.prox;
+				tras.setItem(elemento);
+				
+				
+		   }
+
+		
+		public Object desenfilerar() throws Exception {
+				Object valor = null;
+				if(this.vazia()){
+					throw new Exception ("error: fila vazia!!");
+					}
+				this.frente=this.frente.prox;
+				valor=this.frente.getItem();
+				
+				return valor;
+					
 				}
-			this.frente=this.frente.prox;
-			valor=this.frente.getValor();
-			
-			return valor;
+				
+				
+		
+
+		public int tamanho(){
+				Celula aux = frente;
+				int i;
+					for(i = 0; aux != null; i++){
+					
+			}
+					return i-1;
+			}
+
+		
+
+		public void imprima() {
+					Celula aux = frente;
+				
+				int i = 0, t = tamanho();
+				System.out.print("||");
+				while(i < t) {
+					System.out.print(aux.prox.getItem());
+					aux = aux.prox;
+					if(aux.prox == null) {
+						System.out.println("||\n");
+			}else{
+						System.out.print(" -- ");
+			}
+						i++;
+			}
 				
 			}
-			
-			
-	
 
-	public int tamanho(){
-			Celula aux = frente;
-			int i;
-				for(i = 0; aux != null; i++){
-				aux = aux.getProx();
-		}
-				return i-1;
-		}
-
-	
-
-	public void imprima() {
-				Celula aux = frente;
-			
-			int i = 0, t = tamanho();
-			System.out.print("||");
-			while(i < t) {
-				System.out.print(aux.getProx().getValor());
-				aux = aux.getProx();
-				if(aux.getProx() == null) {
-					System.out.println("||\n");
-		}else{
-					System.out.print(" -- ");
-		}
-					i++;
-		}
-			
-		}
-
-		public boolean vazia(){
-			if(tamanho() <= 1){
-				return true;
-		}
-			return false;
-		}
-		
-		}
-
-
+			public boolean vazia(){
+				if(tamanho() <= 1){
+					return true;
+			}
+				return false;
+			}
+}
