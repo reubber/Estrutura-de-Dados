531dca1ad395b66443d259691d7d390c0e4de860
rBBr <reubbers@gmail.com>
2016-10-06 16:19:02 -0300

    Create Celula.java

---
+++ Trabalhos_ED/src/Filas_autoreferenciadas/Celula.java
@@ -0,0 +1,20 @@
+package cap3.rbbr;
+
+public class Celula {
+	
+	private Object item;
+	Celula prox;
+	
+	
+	
+	
+	public Object getItem() {
+		return item;
+	}
+	public void setItem(Object item) {
+		this.item = item;
+	}
+	
+	
+
+}
