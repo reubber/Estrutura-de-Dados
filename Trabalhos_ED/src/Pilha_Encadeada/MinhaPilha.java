699927b84ae667f4b937236d921f4be71c6f1e25
rBBr <reubbers@gmail.com>
2016-10-06 14:10:23 -0300

    Create MinhaPilha.java

---
+++ Trabalhos_ED/src/Pilha_Encadeada/MinhaPilha.java
@@ -0,0 +1,47 @@
+package tipos.estruturados.pilhasEncadeadas.rbbr.ufc;
+
+public class MinhaPilha {
+	
+	private static class Celula {
+		Object item;
+		Celula prox;
+		
+	}
+	private Celula topo;
+	private int tam;
+		public Pilhas (){
+			this.topo=null;
+				this.tam=0;
+		
+		
+	}
+		
+		
+	public void empilhar (Object elemento){
+		
+		Celula aux = this.topo;
+		this.topo= new Celula();
+		this.topo.item = elemento;
+		this.topo.prox= aux;
+		
+		
+	}
+	
+	public Object desempilhar() throws Exception {
+		if (this.vazia())
+				throw new Exception("Ta mt errado: Pilha vazia!!");
+		Object item = this.topo.item;
+		this.topo = this.topo.prox;
+		this.tam--;
+		return item;
+	}
+	
+	public boolean vazia() {
+		return (this.topo==null);
+	}
+	
+	public int tamanho() {
+		return this.tam;
+	}
+
+}
